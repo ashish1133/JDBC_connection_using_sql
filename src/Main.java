20e8fdfa592a1b53001eee5d20c0350d4507210d
priya <ashishashok893@gmail.com>
2024-07-08 18:10:54 +0530

    JBBC connection on MySql

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,31 @@
-class mergesort{
-    void merge(int[] arr,int l,int m ,int r){
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-        int n1 = m-l+1;
-        int n2 = r-m;
+// its working properly//
 
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i=0;i<n1;++i)
-            L[i]=arr[l+i];
-        for (int j=0;j<n2;++j)
-            R[j]=arr[m+1+j];
-
-        int i=0,j=0;
-
-        int k = l;
-        while (i<n1 && j<n1){
-            if (L[i]<=R[j]){
-                arr[k]=L[i];
-                i++;
-            }else {
-                arr[k]=R[j];
-                j++;
-            }k++;
-        }
-
-        while (i<n1){
-            arr[k]=L[i];
-            i++;
-            k++;
-        }
-        while (j<n2){
-            arr[k]=R[j];
-            j++;
-            k++;
-        }
-
-    }
-    void sort(int[] arr,int l,int r){
-
-        if(l<r){
-            int m = l+(r-l)/2;
-
-            sort(arr, l, m);
-            sort(arr,m+1,r);
-
-            merge(arr,l,m,r);
-
-        }
-
-    }
-    static void printArray(int[] arr){
-        int n =arr.length;
-        for (int i=0;i<n;++i){
-            System.out.print(arr[i]+" ");
-        }
-    }
-
+class main{
     public static void main(String[] args) {
 
-        int[] arr= {22,76,13,65,34,90,87,43,2};
+        String url ="jdbc:mysql://localhost:3306/your Database name here";
+        String user = "root";
+        String password ="password";
+        String Qurys ="Select* from table";
 
-        mergesort o = new mergesort();
-        o.sort(arr,0, arr.length-1);
-        printArray(arr);
+        try(
+                Connection con = DriverManager.getConnection(url,user,password);
+                Statement  st =con.createStatement();
+                ResultSet rs = st.executeQuery(Qurys);
+                )
+        {
+            System.out.println(rs.getInt(1));
+            System.out.println(rs.getInt(2));
+            System.out.println(rs.getInt(3));
+
+        }
+        catch (Exception p){
+            p.printStackTrace();
+        }
     }
 }
